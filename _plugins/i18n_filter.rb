b92543c2a6e76ff4cb7a88fb8b4386b3efa196d5
Álex G.M <thefirered@gmail.com>
2016-08-05 01:52:52 +0200

    Revert "Localize"
    This reverts commit b9b909b03d9a3ced68228be5eb1dd33b681e8384.

--- _plugins/i18n_filter.rb
+++ _plugins/i18n_filter.rb
@@ -1,10 +1,11 @@
 require 'i18n'
 
-LOCALE = Jekyll.configuration({})['es-ES'] # set your locale from config var
+LOCALE = :es-ES # set your locale
 
 # Create folder "_locales" and put some locale file from https://github.com/svenfuchs/rails-i18n/tree/master/rails/locale
 module Jekyll
   module I18nFilter
+
     # Example:
     #   {{ post.date | localize: "%d.%m.%Y" }}
     #   {{ post.date | localize: ":short" }}
@@ -12,12 +13,10 @@ module Jekyll
       load_translations
       format = (format =~ /^:(\w+)/) ? $1.to_sym : format
       I18n.l input, :format => format
-    rescue
-      "error"
     end
 
     def load_translations
-      if I18n.backend.send(:translations).empty?
+      unless I18n::backend.instance_variable_get(:@translations)
         I18n.backend.load_translations Dir[File.join(File.dirname(__FILE__),'../_locales/*.yml')]
         I18n.locale = LOCALE
       end
